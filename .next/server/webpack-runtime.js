037d3346a5cd6c5efe86c1d7623770ef509bc311
Adriano Ronszcka <ronszcka@gmail.com>
2023-02-12 16:29:11 -0300

    página de obrigado

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("52a4af918971ddba")
+/******/ 		__webpack_require__.h = () => ("5912983eac75794b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- src/app/obrigada/page.tsx
+++ src/app/obrigada/page.tsx
@@ -1,7 +1,39 @@
+import './styles.css'
+
+import Image from 'next/image'
+
 export default async function Obrigada() {
   return (
-    <div>
-      <h1>Obrigada</h1>
-    </div>
-  )
+    <>
+      <main>
+        <section className="section-hero">
+          <div className="hero">
+            <div className="hero-text-box">
+              <h1 className="heading-primary">
+                Obrigada pelo interesse na avaliação!
+              </h1>
+              <p className="hero-description">
+                Já salvamos seu contato e logo iremos entrar em contato contigo para agendar o seu horário!
+              </p>
+              <p className="hero-description">
+                Te aguado em breve em meu consultório para tirar todas as suas dúvidas e te deixar ainda mais linda!
+              </p>
+              <a href="https://bit.ly/whatsapp_ej_estetica" target="_blank" className="btn btn--full margin-right-sm">
+                Chamar no whatsapp!
+              </a>
+            </div>
+            <div className="hero-img-box">
+              <Image
+                className="hero-img"
+                src="/img/fotos/dra-jociene-andrade.jpg"
+                alt="Foto da Dra. Jociene em sua mesa no momento da avaliação"
+                width={1024}
+                height={1364}
+              />
+            </div>
+          </div>
+        </section>
+        </main>
+        </>
+  );
 }
