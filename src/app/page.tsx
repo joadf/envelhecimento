08b0072f00884939999a133af15a972f3a9c5b73
Adriano Ronszcka <ronszcka@gmail.com>
2023-02-12 17:21:50 -0300

    pixel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,23 @@ export default function Home() {
         src="https://www.googletagmanager.com/gtag/js?id=G-SW67Q3WR3D"
         strategy="afterInteractive"
       />
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '5257881957569181');
+          fbq('track', 'PageView');
+          <noscript><img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=5257881957569181&ev=PageView&noscript=1"
+          /></noscript>
+        `}
+      </Script>
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
